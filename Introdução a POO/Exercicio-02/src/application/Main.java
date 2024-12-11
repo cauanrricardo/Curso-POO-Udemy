239cb34a2fb264ad4d29368bd61a219cfb11834d
Cauan Ricardo <cauanrricardo@gmail.com>
2024-12-11 18:30:12 -0300

    Atualizacao do exercicio 02 - Introducao a POO

---
+++ Introdução a POO/Exercicio-02/src/application/Main.java
@@ -0,0 +1,30 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+package application;
+
+import entities.Employee;
+import java.util.Scanner;
+
+public class Main {
+   public Main() {
+   }
+
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      Employee emp = new Employee();
+      System.out.println("Name: ");
+      emp.name = sc.nextLine();
+      System.out.println("Gross salary: ");
+      emp.grossSalary = sc.nextDouble();
+      System.out.println("Tax: ");
+      emp.tax = sc.nextDouble();
+      System.out.println();
+      System.out.println("Employee: " + String.valueOf(emp));
+      System.out.println();
+      System.out.println("Which percentage to increase salary? ");
+      double percentage = sc.nextDouble();
+      emp.increaseSalary(percentage);
+      System.out.println();
+      System.out.println("Update data: " + String.valueOf(emp));
+      sc.close();
+   }
+}
---
+++ Introdução a POO/Exercicio-02/src/entities/Employee.java
@@ -0,0 +1,24 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+package entities;
+
+public class Employee {
+   public String name;
+   public double grossSalary;
+   public double tax;
+
+   public Employee() {
+   }
+
+   public double netSalary() {
+      return this.grossSalary - this.tax;
+   }
+
+   public void increaseSalary(double porcentage) {
+      this.grossSalary *= 1.0 + porcentage / 100.0;
+   }
+
+   public String toString() {
+      String var10000 = this.name;
+      return var10000 + ", $" + String.format("%.2f", this.netSalary());
+   }
+}
